bb29759ac0b0d952579f4ebaa8e6baf50fc50423
luisbelisario <luisbls77@hotmail.com>
2022-01-11 10:23:27 -0300

    Finalizado Moviecard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,30 @@
 import Pagination from "components/Pagination";
+import Moviecard from "components/Moviecard";
 
 function Listing() {
-    return(
-        <Pagination />
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Moviecard />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
