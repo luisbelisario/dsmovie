6b39ff64b586d073a0205f5a43e07078d9a45e23
luisbelisario <luisbls77@hotmail.com>
2022-01-13 16:54:14 -0300

    Correcoes para a producao frontend

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
         <nav className="container">
           <div className="dsmovie-nav-content">
             <h1>DS Movie</h1>
-            <a href="https://github.com/luisbelisario" target='_blank' className='dsmovie-link-container'>
+            <a href="https://github.com/luisbelisario" className='dsmovie-link-container'>
               <div className='dsmovie-contact-container'>
                 <GitHubIcon />
                 <p className='ds-movie-contact-link'>/luisbelisario</p>
