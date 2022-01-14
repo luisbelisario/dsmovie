77a39be4b3e863c99e85e44c65c887c3e215ae72
luisbelisario <luisbls77@hotmail.com>
2022-01-14 16:43:56 -0300

    Show scores

--- frontend/src/components/Moviescore/index.tsx
+++ frontend/src/components/Moviescore/index.tsx
@@ -1,16 +1,18 @@
 import Moviestars from "components/Moviestars";
 import './moviescore.css';
 
+type Props = {
+    score: number;
+    count: number;
+}
 
-function Moviescore() {
-    
-    const score = 3.5;
-    const count = 13;
+
+function Moviescore( { score, count } : Props ) {
     
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <Moviestars />
+            <Moviestars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     )
